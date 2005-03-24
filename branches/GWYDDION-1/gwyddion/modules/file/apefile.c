6951a5a7461646dcd0e3802a56cddf8d3d40231f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-03-24 15:46:53 +0000

    - implemented basic data selection GUI and added some metadata
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@3601 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-1/gwyddion/modules/file/apefile.c
+++ branches/GWYDDION-1/gwyddion/modules/file/apefile.c
@@ -20,9 +20,11 @@
 #define DEBUG 1
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwyutils.h>
-#include <libgwyddion/gwymath.h>
 #include <libgwymodule/gwymodule.h>
 #include <libprocess/datafield.h>
+#include <libgwydgets/gwyradiobuttons.h>
+#include <libgwydgets/gwydataview.h>
+#include <libgwydgets/gwylayer-basic.h>
 #include <app/gwyapp.h>
 
 #include <string.h>
@@ -80,6 +82,12 @@ typedef struct {
     GwyDataField **data;
 } APEFile;
 
+typedef struct {
+    APEFile *file;
+    GwyContainer *data;
+    GtkWidget *data_view;
+} APEControls;
+
 static gboolean      module_register    (const gchar *name);
 static gint          apefile_detect     (const gchar *filename,
                                          gboolean only_name);
@@ -88,6 +96,17 @@ static void          fill_data_fields   (APEFile *apefile,
                                          const guchar *buffer);
 static void          store_metadata     (APEFile *apefile,
                                          GwyContainer *container);
+static guint         select_which_data  (APEFile *apefile);
+static void          selection_changed  (GtkWidget *button,
+                                         APEControls *controls);
+
+static const GwyEnum spm_modes[] = {
+    { "SNOM",                  SPM_MODE_SNOM },
+    { "AFM Non-contact",       SPM_MODE_AFM_NONCONTACT },
+    { "AFM Contact",           SPM_MODE_AFM_CONTACT },
+    { "STM",                   SPM_MODE_STM },
+    { "Phase detection AFM",   SPM_MODE_PHASE_DETECT_AFM },
+};
 
 /* The module info. */
 static GwyModuleInfo module_info = {
@@ -264,15 +283,22 @@ apefile_load(const gchar *filename)
         }
     }
     fill_data_fields(&apefile, p);
-    /* XXX */
-    dfield = apefile.data[0];
-
     gwy_file_abandon_contents(buffer, size, NULL);
 
-    object = gwy_container_new();
-    gwy_container_set_object_by_name(GWY_CONTAINER(object), "/0/data",
-                                     G_OBJECT(dfield));
-    store_metadata(&apefile, GWY_CONTAINER(object));
+    n = select_which_data(&apefile);
+    if (n != (guint)-1)
+        dfield = apefile.data[n];
+
+    if (dfield) {
+        object = gwy_container_new();
+        gwy_container_set_object_by_name(GWY_CONTAINER(object), "/0/data",
+                                         G_OBJECT(dfield));
+        store_metadata(&apefile, GWY_CONTAINER(object));
+    }
+    for (b = 0; b < apefile.ndata; b++)
+        g_object_unref(apefile.data[b]);
+
+    g_free(apefile.remark);
 
     return (GwyContainer*)object;
 }
@@ -318,6 +344,125 @@ store_metadata(APEFile *apefile,
     HASH_STORE("Acquire delay", "%.6f s", acquire_delay);
     HASH_STORE("Raster delay", "%.6f s", raster_delay);
     HASH_STORE("Tip distance", "%g nm", tip_dist);
+    HASH_STORE("Comment", "%s", remark);
+    gwy_container_set_string_by_name
+        (container, "/meta/SPM mode",
+         g_strdup(gwy_enum_to_string(apefile->spm_mode, spm_modes,
+                                     G_N_ELEMENTS(spm_modes))));
+}
+
+static guint
+select_which_data(APEFile *apefile)
+{
+    APEControls controls;
+    GtkWidget *dialog, *label, *vbox, *hbox, *align;
+    GwyEnum *choices;
+    GtkObject *layer;
+    GSList *radio, *rl;
+    guint i, b;
+
+    if (!apefile->ndata)
+        return (guint)-1;
+
+    if (apefile->ndata == 1)
+        return 0;
+
+    controls.file = apefile;
+    choices = g_new(GwyEnum, apefile->ndata);
+    b = 0;
+    for (i = 0; i < apefile->ndata; i++) {
+        while (!((1 << b) & apefile->channels))
+            b++;
+        b++;
+        choices[i].value = i;
+        choices[i].name = g_strdup_printf(_("Channel %u"), b);
+    }
+
+    dialog = gtk_dialog_new_with_buttons(_("Select Data"), NULL, 0,
+                                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                         GTK_STOCK_OK, GTK_RESPONSE_OK,
+                                         NULL);
+    gtk_dialog_set_has_separator(GTK_DIALOG(dialog), FALSE);
+    gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+    hbox = gtk_hbox_new(FALSE, 20);
+    gtk_container_set_border_width(GTK_CONTAINER(hbox), 6);
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), hbox, TRUE, TRUE, 0);
+
+    align = gtk_alignment_new(0.0, 0.0, 0.0, 0.0);
+    gtk_box_pack_start(GTK_BOX(hbox), align, TRUE, TRUE, 0);
+
+    vbox = gtk_vbox_new(TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(align), vbox);
+
+    label = gtk_label_new(_("Data to load:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+
+    radio = gwy_radio_buttons_create(choices, apefile->ndata, "data",
+                                     G_CALLBACK(selection_changed), &controls,
+                                     0);
+    for (i = 0, rl = radio; rl; i++, rl = g_slist_next(rl))
+        gtk_box_pack_start(GTK_BOX(vbox), GTK_WIDGET(rl->data), TRUE, TRUE, 0);
+
+    /* preview */
+    align = gtk_alignment_new(1.0, 0.0, 0.0, 0.0);
+    gtk_box_pack_start(GTK_BOX(hbox), align, TRUE, TRUE, 0);
+
+    controls.data = GWY_CONTAINER(gwy_container_new());
+    gwy_container_set_object_by_name(controls.data, "/0/data",
+                                     G_OBJECT(apefile->data[0]));
+
+    controls.data_view = gwy_data_view_new(controls.data);
+    g_object_unref(controls.data);
+    gwy_data_view_set_zoom(GWY_DATA_VIEW(controls.data_view),
+                           120.0/apefile->res);
+    layer = gwy_layer_basic_new();
+    gwy_data_view_set_base_layer(GWY_DATA_VIEW(controls.data_view),
+                                 GWY_PIXMAP_LAYER(layer));
+    gtk_container_add(GTK_CONTAINER(align), controls.data_view);
+
+    gtk_widget_show_all(dialog);
+    gtk_window_present(GTK_WINDOW(dialog));
+    switch (gtk_dialog_run(GTK_DIALOG(dialog))) {
+        case GTK_RESPONSE_CANCEL:
+        case GTK_RESPONSE_DELETE_EVENT:
+        gtk_widget_destroy(dialog);
+        case GTK_RESPONSE_NONE:
+        b = (guint)-1;
+        break;
+
+        case GTK_RESPONSE_OK:
+        b = GPOINTER_TO_UINT(gwy_radio_buttons_get_current(radio, "data"));
+        gtk_widget_destroy(dialog);
+        break;
+
+        default:
+        g_assert_not_reached();
+        break;
+    }
+
+    for (i = 0; i < apefile->ndata; i++)
+        g_free((gpointer)choices[i].name);
+    g_free(choices);
+
+    return b;
+}
+
+static void
+selection_changed(GtkWidget *button,
+                  APEControls *controls)
+{
+    guint i;
+
+    if (!gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)))
+        return;
+
+    i = gwy_radio_buttons_get_current_from_widget(button, "data");
+    g_assert(i != (guint)-1);
+    gwy_container_set_object_by_name(controls->data, "/0/data",
+                                     G_OBJECT(controls->file->data[i]));
+    gwy_data_view_update(GWY_DATA_VIEW(controls->data_view));
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
