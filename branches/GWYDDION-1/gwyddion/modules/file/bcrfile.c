a89f552b613546b9acfe297c927aaee5e8ffad21
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-03-25 21:45:50 +0000

    - added metadata, somewhat reorganized code
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@3611 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-1/gwyddion/modules/file/bcrfile.c
+++ branches/GWYDDION-1/gwyddion/modules/file/bcrfile.c
@@ -17,7 +17,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
-#define DEBUG 1
+
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwyutils.h>
 #include <libgwymodule/gwymodule.h>
@@ -47,19 +47,25 @@ static gboolean      module_register       (const gchar *name);
 static gint          bcrfile_detect        (const gchar *filename,
                                             gboolean only_name);
 static GwyContainer* bcrfile_load          (const gchar *filename);
+static GwyDataField* file_load_real        (const guchar *buffer,
+                                            gsize size,
+                                            GHashTable *meta);
 static GwyDataField* read_data_field       (const guchar *buffer,
                                             gint xres,
                                             gint yres,
                                             BCRFileType type,
                                             gboolean little_endian);
-static GHashTable*   load_metadata         (gchar *buffer);
+static void          load_metadata         (gchar *buffer,
+                                            GHashTable *meta);
+static void          store_metadata        (GHashTable *meta,
+                                            GwyContainer *container);
 
 /* The module info. */
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
     &module_register,
     "bcrfile",
-    N_("Imports Imagemet BCR data files."),
+    N_("Imports Image Metrology BCR data files."),
     "Yeti <yeti@gwyddion.net>",
     "0.1",
     "David Nečas (Yeti) & Petr Klbcrtek",
@@ -116,17 +122,12 @@ bcrfile_detect(const gchar *filename, gboolean only_name)
 static GwyContainer*
 bcrfile_load(const gchar *filename)
 {
-    BCRFileType type;
     GObject *object = NULL;
     guchar *buffer = NULL;
-    guchar *s;
     gsize size = 0;
     GError *err = NULL;
     GwyDataField *dfield = NULL;
     GHashTable *meta = NULL;
-    gint xres, yres;
-    gboolean intelmode = TRUE;
-    gdouble q;
 
     if (!gwy_file_get_contents(filename, &buffer, &size, &err)) {
         g_warning("Cannot read file %s", filename);
@@ -139,30 +140,61 @@ bcrfile_load(const gchar *filename)
         return NULL;
     }
 
-    if (!memcmp(buffer, MAGIC1, MAGIC_SIZE1))
-        type = BCR_FILE_INT16;
-    else if (!memcmp(buffer, MAGIC2, MAGIC_SIZE2))
-        type = BCR_FILE_FLOAT;
-    else {
-        g_warning("Cannot understand file type header");
-        goto end;
+    meta = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
+    dfield = file_load_real(buffer, size, meta);
+    gwy_file_abandon_contents(buffer, size, NULL);
+    if (dfield) {
+        object = gwy_container_new();
+        gwy_container_set_object_by_name(GWY_CONTAINER(object), "/0/data",
+                                         G_OBJECT(dfield));
+        store_metadata(meta, GWY_CONTAINER(object));
     }
-    gwy_debug("File type: %u", type);
+    g_hash_table_destroy(meta);
+
+    return (GwyContainer*)object;
+}
+
+static GwyDataField*
+file_load_real(const guchar *buffer,
+               gsize size,
+               GHashTable *meta)
+{
+    GwyDataField *dfield;
+    gboolean intelmode = TRUE;
+    BCRFileType type;
+    gdouble q;
+    gint xres, yres;
+    guchar *s;
 
     s = g_memdup(buffer, HEADER_SIZE);
     s[HEADER_SIZE-1] = '\0';
-    meta = load_metadata(s);
+    load_metadata(s, meta);
     g_free(s);
+
+    if (!(s = g_hash_table_lookup(meta, "fileformat"))) {
+        g_warning("File is not a BCR file");
+        return NULL;
+    }
+
+    if (!strcmp(s, "bcrstm"))
+        type = BCR_FILE_INT16;
+    else if (!strcmp(s, "bcrf"))
+        type = BCR_FILE_FLOAT;
+    else {
+        g_warning("Cannot understand file type header `%s'", s);
+        return NULL;
+    }
+    gwy_debug("File type: %u", type);
 
     if (!(s = g_hash_table_lookup(meta, "xpixels"))) {
         g_warning("No xpixels (x resolution) info");
-        goto end;
+        return NULL;
     }
     xres = atol(s);
 
     if (!(s = g_hash_table_lookup(meta, "ypixels"))) {
         g_warning("No ypixels (y resolution) info");
-        goto end;
+        return NULL;
     }
     yres = atol(s);
 
@@ -172,7 +204,7 @@ bcrfile_load(const gchar *filename)
     if (size < HEADER_SIZE + xres*yres*type) {
         g_warning("Expected data size %u, but it's %u",
                   xres*yres*type, (guint)(size - HEADER_SIZE));
-        goto end;
+        return NULL;
     }
 
     dfield = read_data_field(buffer + HEADER_SIZE, xres, yres,
@@ -181,25 +213,57 @@ bcrfile_load(const gchar *filename)
     if ((s = g_hash_table_lookup(meta, "xlength"))
         && (q = g_ascii_strtod(s, NULL)) > 0)
         gwy_data_field_set_xreal(dfield, 1e-9*q);
+
     if ((s = g_hash_table_lookup(meta, "ylength"))
         && (q = g_ascii_strtod(s, NULL)) > 0)
         gwy_data_field_set_yreal(dfield, 1e-9*q);
+
     if (type == BCR_FILE_INT16
         && (s = g_hash_table_lookup(meta, "bit2nm"))
         && (q = g_ascii_strtod(s, NULL)) > 0)
         gwy_data_field_multiply(dfield, 1e-9*q);
 
-    object = gwy_container_new();
-    gwy_container_set_object_by_name(GWY_CONTAINER(object), "/0/data",
-                                     G_OBJECT(dfield));
-    /*store_metadata(&bcrfile, GWY_CONTAINER(object));*/
+    return dfield;
+}
 
-end:
-    gwy_file_abandon_contents(buffer, size, NULL);
-    if (meta)
-        g_hash_table_destroy(meta);
+static void
+store_metadata(GHashTable *meta,
+               GwyContainer *container)
+{
+    const struct {
+        const gchar *id;
+        const gchar *unit;
+        const gchar *key;
+    }
+    metakeys[] = {
+        { "scanspeed",   "nm/s",    "Scan speed"        },
+        { "xoffset",     "nm",      "X offset"          },
+        { "yoffset",     "nm",      "Y offset"          },
+        { "bias",        "V",       "Bias voltage"      },
+        { "current",     "nA",      "Tunneling current" },
+        { "starttime",   NULL,      "Scan time"         },
+        /* FIXME: I've seen other stuff, but don't know interpretation */
+    };
+    gchar *value;
+    GString *key;
+    guint i;
 
-    return (GwyContainer*)object;
+    key = g_string_new("");
+    for (i = 0; i < G_N_ELEMENTS(metakeys); i++) {
+        if (!(value = g_hash_table_lookup(meta, metakeys[i].id)))
+            continue;
+
+        g_string_printf(key, "/meta/%s", metakeys[i].key);
+        if (metakeys[i].unit)
+            gwy_container_set_string_by_name(container, key->str,
+                                             g_strdup_printf("%s %s",
+                                                             value,
+                                                             metakeys[i].unit));
+        else
+            gwy_container_set_string_by_name(container, key->str,
+                                             g_strdup(value));
+    }
+    g_string_free(key, TRUE);
 }
 
 static GwyDataField*
@@ -251,14 +315,12 @@ read_data_field(const guchar *buffer,
     return dfield;
 }
 
-static GHashTable*
-load_metadata(gchar *buffer)
+static void
+load_metadata(gchar *buffer,
+              GHashTable *meta)
 {
     gchar *line, *p;
-    GHashTable *meta;
     gchar *key, *value;
-
-    meta = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
     while ((line = gwy_str_next_line(&buffer))) {
         if (line[0] == '%' || line[0] == '#')
@@ -281,9 +343,8 @@ load_metadata(gchar *buffer)
         }
 
         g_hash_table_insert(meta, key, value);
+        gwy_debug("<%s> = <%s>", key, value);
     }
-
-    return meta;
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
