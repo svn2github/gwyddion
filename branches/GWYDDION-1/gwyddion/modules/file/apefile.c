5c7af352a0466b503908534e6324c798bd200e00
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-03-25 10:58:32 +0000

    - added Wine copyright information
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@3607 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-1/gwyddion/modules/file/apefile.c
+++ branches/GWYDDION-1/gwyddion/modules/file/apefile.c
@@ -3,6 +3,8 @@
  *  Copyright (C) 2005 David Necas (Yeti), Petr Klapetek.
  *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
  *
+ *  Date conversion code copied from Wine, see below.
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -483,6 +485,24 @@ selection_changed(GtkWidget *button,
 }
 
 /******************** Wine date conversion code *****************************/
+
+/*
+ * Copyright (C) the Wine project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 typedef guint WORD;
 typedef guint USHORT;
