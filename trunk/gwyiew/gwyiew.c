af8a36e18aab410ff518022372be9410a0b61583
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-04-02 22:56:16 +0000

    - changed copyright to myself - removed crap from readme
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@3653 25697fe4-d644-49b8-9d30-be5465f2adf3

--- trunk/gwyiew/gwyiew.c
+++ trunk/gwyiew/gwyiew.c
@@ -1,7 +1,7 @@
 /*
  *  @(#) $Id$
- *  Copyright (C) 2003,2004 David Necas (Yeti), Petr Klapetek.
- *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
+ *  Copyright (C) 2004,2005 David Necas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
