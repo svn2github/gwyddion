e9d0ce2521612c00e45fdcd0224829de728970ec
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-11-19 10:56:44 +0000

    - fixed more cases of Petr's name mangling
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@5019 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-1/gwyddion/modules/file/rhk-spm32.c
+++ branches/GWYDDION-1/gwyddion/modules/file/rhk-spm32.c
@@ -1,7 +1,7 @@
 /*
  *  $Id$
  *  Copyright (C) 2005 David Necas (Yeti), Petr Klapetek.
- *  E-mail: yeti@gwyddion.net, klrhktek@gwyddion.net.
+ *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
