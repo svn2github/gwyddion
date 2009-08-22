7e130ba6f5d77de773c88bcc4f8ddea11d0ef96c
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 19:17:57 +0000

    - added GSL non-linear least-squares fitting code
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9993 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/libgwy/nlfit/nlfit.h
@@ -0,0 +1,15 @@
+#ifndef __GWY_GSL_NLFIT_H__
+#define __GWY_GSL_NLFIT_H__ 1
+
+#include <stdlib.h>
+#include <math.h>
+#include "gsl_errno.h"
+#include "gsl_block.h"
+#include "gsl_vector.h"
+#include "gsl_matrix.h"
+#include "gsl_blas.h"
+#include "gsl_permutation.h"
+#include "gsl_linalg.h"
+#include "gsl_multifit_nlin.h"
+
+#endif
