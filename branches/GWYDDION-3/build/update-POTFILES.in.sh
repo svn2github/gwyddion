39e49a7ea313a4191a8a5d9bd94f67c4a5cb26ae
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 18:04:09 +0000

    - added POTFILES.in update script (really updates, i.e. does not touch the file   if nothing changes)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9707 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/build/update-POTFILES.in.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+outfile=po/POTFILES.in
+
+# Keep the `GENERATED' string separated to prevent match here
+G=GENERATED
+cat >$outfile.new <<EOF
+# List of source files containing translatable strings.
+# This is a $G file, by build/update-POTFILES.in.sh.
+EOF
+
+for dir in libgwy; do
+  echo >>$outfile.new
+  echo "# $dir" >>$outfile.new
+  find $dir -name \*.\[ch\] \
+    | xargs egrep -l '\<[QNC]?_\(|\<[a-z_]{,5}gettext\(' \
+    | sort >>$outfile.new
+done
+
+if test -f $outfile && diff -q $outfile $outfile.new >/dev/null; then
+  rm -f $outfile.new
+else
+  mv -f $outfile.new $outfile
+fi
