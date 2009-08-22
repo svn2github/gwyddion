6f8d756e8f5342cf0b7d836f1657aee2c1906593
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 18:26:25 +0000

    - added data subdirectory with gwyddion.vim rules
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9775 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/data/gwyddion-vim-cfg.py
@@ -0,0 +1,5 @@
+from os import getcwd as _getcwd
+file_glob = _getcwd() + '/../docs/*/*-decl.txt'
+maintainer = 'Yeti <yeti@gwyddion.net>'
+syntax_name = 'gwyddion'
+url = 'http://gwyddion.net/download/gwyddion.vim'
