68dfda59f808ea477c84777b335e0747fef5ff4a
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 18:59:14 +0000

    - outlined search path setup plan
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9915 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/libgwy.c
+++ branches/GWYDDION-3/libgwy/libgwy.c
@@ -54,6 +54,76 @@ init_types(G_GNUC_UNUSED gpointer arg)
     return NULL;
 }
 
+/*
+ * Finding directiores.
+ *
+ * We deliberately ignore XDG which unfortunately means ignoring most GLib
+ * provided functions.  Rationale:
+ * - Doing things the Unix way has preference to some piece of PDF.
+ * - The difference between configuration and data is artificial.
+ * - How do I get a search path for loadable modules from the Base Directory
+ *   Specification?  And if I cannot, how am I supposed to be consistent then?
+ * - It does not seem that openssh, bash or vim are going to follow some
+ *   XDG_THINGS_IN_WEIRD_PLACES variables.
+ *
+ * 0. OVERVIEW
+ * We have the following primary tasks:
+ *
+ * 1. Find self.  This means the directory this library was actually loaded
+ * from.
+ *
+ * 2. Find the user configuration directory.  This is in home, or in other
+ * place if we are on Win32 and other weird systems.
+ *
+ * 3. Find all directories where we should look for "foo".  These can be of
+ * the following kinds:
+ *
+ *   (a) Related to the system directory, defined in point 1.
+ *
+ *   (b) Related to the user directory, defined in point 2.
+ *
+ *   (c) Arbitrary, unrelated to anything.
+ *
+ * 1. SEARCHING SELF
+ * The following can be tried:
+ * - environment variables GWYDDION_DATADIR, GWYDDION_LIBDIR,
+ *   GWYDDION_LOCALE_DIR
+ * - /proc/self/map (only on Linux)
+ * - g_win32_get_package_installation_directory_of_module() (only on Win32)
+ * - argv[0] and realpath, possibly together with PATH
+ * - compile-time prefix
+ *
+ * These possibilities are tried in the following order and the first that
+ * results in finding self is used.  The order is given by decreasing
+ * reliability, except of course GWYDDION_ROOTDIR, which is not very reliable
+ * but the user it always right.
+ *
+ * If none of them produces a match, fail.  This means that a fool-proof user
+ * of libgwy must check whether gwy_data_directory(), gwy_lib_directory(),
+ * gwy_locale_directory() return non-NULL.
+ *
+ * 2. SEARCHING USER DIRECTORY
+ * The following can be tried:
+ * - environment variable HOME
+ * - g_get_home_dir()
+ *
+ * If neither leads to a suitable directory, fail.  An application should give
+ * a warning if it find that gwy_user_directory() returns NULL and avoid
+ * saving configuration.  It needs to check whether it actualy succeeded
+ * anyway.
+ *
+ * 3. GATHERING ALL DIRECTORIES
+ * The following can be tried:
+ * - specific environment variables with modules, data, etc. paths -- not
+ *   implemented (c)
+ * - user directory (b)
+ * - our installation directory (a)
+ * - default system directories (such as /usr) (c)
+ *
+ * All are concatenated into one list in given order.  If some directory
+ * happens to occur multiple times the first occurrence counts.
+ */
+
 #define __GWY_LIBGWY_C__
 #include "libgwy/libgwy-aliases.c"
 
