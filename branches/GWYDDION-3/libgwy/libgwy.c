93afdad6f6dec738ae5c8eea111cb603a03de937
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 18:52:28 +0000

    - added documentation for libgwy
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9887 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/libgwy.c
+++ branches/GWYDDION-3/libgwy/libgwy.c
@@ -57,4 +57,10 @@ init_types(G_GNUC_UNUSED gpointer arg)
 #define __GWY_LIBGWY_C__
 #include "libgwy/libgwy-aliases.c"
 
+/**
+ * SECTION: libgwy
+ * @title: libgwy
+ * @short_description: Library-level functions of libgwy
+ **/
+
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
