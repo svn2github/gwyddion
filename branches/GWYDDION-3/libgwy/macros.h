e8d3939040fdb417540d781c206c1629a52f003b
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 17:59:16 +0000

    - added idempotent macros GWY_FREE, GWY_UNREF, GWY_SIGNAL_HANDLER_DISCONNECT
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9688 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/macros.h
+++ branches/GWYDDION-3/libgwy/macros.h
@@ -23,6 +23,17 @@
 #define GWY_SWAP(t, x, y) \
     do { t ___gwy_swap; ___gwy_swap = x; x = y; y = ___gwy_swap; } while (0)
 
+#define GWY_FREE(ptr) \
+    do { if (ptr) { g_free(ptr); (ptr) = NULL; } } while (0)
+
+#define GWY_OBJECT_UNREF(obj) \
+    do { if (obj) { g_object_unref(obj); (obj) = NULL; } } while (0)
+
+#define GWY_SIGNAL_HANDLER_DISCONNECT(obj, hid) \
+    do { \
+        if (hid && obj) { g_signal_handler_disconnect(obj, hid); (hid) = 0; } \
+    } while (0)
+
 #endif
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
