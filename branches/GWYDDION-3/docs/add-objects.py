f170c030eb4c68877e76c91ab2afbb554e321320
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-22 18:28:19 +0000

    - read interfaces file and try to add interfaces too without breaking classes
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@9783 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/docs/add-objects.py
+++ branches/GWYDDION-3/docs/add-objects.py
@@ -3,27 +3,20 @@
 from __future__ import generators
 import sys, re
 
-debug = False
+debug = True
 
-# Compatibility with Pyhton-2.2
-if not __builtins__.__dict__.has_key('enumerate'):
-    def enumerate(collection):
-        i = 0
-        it = iter(collection)
-        while True:
-            yield i, it.next()
-            i += 1
-
-if len(sys.argv) < 3:
-    print sys.argv[0], 'SECTION_FILE OBJECT_FILE [--standard-files=FILE,...]'
+if len(sys.argv) < 4:
+    print sys.argv[0], 'SECTION_FILE OBJECT_FILE INTERFACE-FILE [--standard-files=FILE,...]'
     sys.exit(1)
 
 title_re = re.compile(r'<TITLE>(?P<object>\w+)</TITLE>')
 file_re = re.compile(r'<FILE>(?P<file>\w+)</FILE>')
+classlike_re = re.compile(r'^\w+(?:Class|Interface)$')
 section_file = sys.argv[1]
 object_file = sys.argv[2]
+interface_file = sys.argv[3]
 ignore_files = {}
-for x in sys.argv[3:]:
+for x in sys.argv[4:]:
     if x.startswith('--standard-files='):
         for f in x[len('--standard-files='):].split(','):
             ignore_files[f] = 1
@@ -31,11 +24,19 @@ for x in sys.argv[3:]:
         stderr.write(sys.argv[0] + ': Unknown option ' + x + '\n')
 
 fh = file(object_file, 'r')
-objects = dict([(s.strip(), 1) for s in fh.readlines()])
+objects = set(s.strip() for s in fh.readlines())
 fh.close()
 
 if debug:
     print 'Objects from %s:' % object_file, objects
+
+# FIXME: This catches only implemented interfaces
+fh = file(interface_file, 'r')
+interfaces = set(s.split()[1] for s in fh.readlines() if s.strip())
+fh.close()
+
+if debug:
+    print 'Interfaces from %s:' % interface_file, interfaces
 
 fh = file(section_file, 'r')
 lines = fh.readlines()
@@ -47,17 +48,22 @@ added = False
 standardizing = False
 for i, l in enumerate(lines):
     m = file_re.match(l)
-    if m and ignore_files.has_key(m.group('file')):
+    if m and m.group('file') in ignore_files:
         standardizing = True
-    if l.strip() == addme or l.strip() == addme + 'Class':
+    if l.strip() in (addme, addme + 'Class', addme + 'Interface') \
+       or classlike_re.match(l.strip()):
         if debug:
             print 'Skipping matching %s' % l.strip()
         l = ''
     if addme and not added:
+        if addme in interfaces:
+            suffix = 'Interface'
+        else:
+            suffix = 'Class'
         if debug:
-            print 'Adding %s, %sClass' % (addme, addme)
+            print 'Adding %s, %s%s' % (addme, addme, suffix)
         fh.write(addme + '\n')
-        fh.write(addme + 'Class\n')
+        fh.write(addme + suffix + '\n')
         added = True
     m = title_re.match(l)
     if m:
@@ -65,7 +71,7 @@ for i, l in enumerate(lines):
         addme = m.group('object')
         if debug:
             print 'Object-like declaration %s' % addme
-        if not objects.has_key(addme):
+        if addme not in objects:
             if debug:
                 print 'Type %s is not in objects' % addme
             addme = ''
