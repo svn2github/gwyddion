a660d3d09490f9ecc3e18d5c16d0ade99ff3c8d4
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-12-04 10:10:09 +0000

    - added tests for matrix inversion
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@10654 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/testlibgwy.c
+++ branches/GWYDDION-3/libgwy/testlibgwy.c
@@ -437,6 +437,106 @@ test_math_cholesky(void)
 
 /***************************************************************************
  *
+ * Linear algebra
+ *
+ ***************************************************************************/
+
+/* Multiply a vector with a matrix from left. */
+static void
+test_linalg_matvec(gdouble *a, const gdouble *m, const gdouble *v, guint n)
+{
+    for (guint i = 0; i < n; i++) {
+        a[i] = 0.0;
+        for (guint j = 0; j < n; j++)
+            a[i] += m[i*n + j] * v[j];
+    }
+}
+
+/* Multiply two square matrices. */
+static void
+test_linalg_matmul(gdouble *a, const gdouble *d1, const gdouble *d2, guint n)
+{
+    for (guint i = 0; i < n; i++) {
+        for (guint j = 0; j < n; j++) {
+            a[i*n + j] = 0.0;
+            for (guint k = 0; k < n; k++)
+                a[i*n + j] += d1[i*n + k] * d2[k*n + j];
+        }
+    }
+}
+
+/* Note the precision checks are very tolerant as the matrices we generate
+ * are not always well-conditioned. */
+static void
+test_math_linalg(void)
+{
+    guint nmax = 8, niter = 50;
+    GRand *rng = g_rand_new();
+
+    /* Make it realy reproducible. */
+    g_rand_set_seed(rng, 42);
+
+    for (guint n = 1; n < nmax; n++) {
+        /* Use descriptive names for less cryptic g_assert() messages. */
+        gdouble *matrix = g_new(gdouble, n*n);
+        gdouble *unity = g_new(gdouble, n*n);
+        gdouble *decomp = g_new(gdouble, n*n);
+        gdouble *vector = g_new(gdouble, n);
+        gdouble *rhs = g_new(gdouble, n);
+        gdouble *solution = g_new(gdouble, n);
+
+        for (guint iter = 0; iter < niter; iter++) {
+            test_cholesky_make_vector(matrix, n*n, rng);
+            test_cholesky_make_vector(vector, n, rng);
+            gdouble eps;
+
+            /* Solution */
+            test_linalg_matvec(rhs, matrix, vector, n);
+            memcpy(decomp, matrix, n*n*sizeof(gdouble));
+            g_assert(gwy_linalg_solve(decomp, rhs, solution, n));
+            eps = exp10(n - 13.0);
+            for (guint j = 0; j < n; j++) {
+                g_assert_cmpfloat(fabs(solution[j] - vector[j]),
+                                  <=,
+                                  eps * (fabs(solution[j]) + fabs(vector[j])));
+            }
+            /* Inversion */
+            memcpy(unity, matrix, n*n*sizeof(gdouble));
+            g_assert(gwy_linalg_invert(unity, decomp, n));
+            /* Multiplication with inverted must give unity */
+            test_linalg_matmul(unity, matrix, decomp, n);
+            for (guint j = 0; j < n; j++) {
+                g_assert_cmpfloat(fabs(unity[j*n + j] - 1.0), <=, eps);
+                for (guint i = 0; i < n; i++) {
+                    if (i == j)
+                        continue;
+                    g_assert_cmpfloat(fabs(unity[j*n + i]), <=, eps);
+                }
+            }
+            /* Double inversion must give the original */
+            g_assert(gwy_linalg_invert(decomp, unity, n));
+            for (guint j = 0; j < n; j++) {
+                for (guint i = 0; i < n; i++) {
+                    g_assert_cmpfloat(fabs(matrix[j] - unity[j]),
+                                      <=,
+                                      eps * (fabs(matrix[j]) + fabs(unity[j])));
+                }
+            }
+        }
+
+        g_free(vector);
+        g_free(rhs);
+        g_free(solution);
+        g_free(unity);
+        g_free(matrix);
+        g_free(decomp);
+    }
+
+    g_rand_free(rng);
+}
+
+/***************************************************************************
+ *
  * Interpolation
  *
  ***************************************************************************/
@@ -2436,6 +2536,7 @@ main(int argc, char *argv[])
     g_test_add_func("/testlibgwy/pack", test_pack);
     g_test_add_func("/testlibgwy/math/sort", test_math_sort);
     g_test_add_func("/testlibgwy/math/cholesky", test_math_cholesky);
+    g_test_add_func("/testlibgwy/math/linalg", test_math_linalg);
     g_test_add_func("/testlibgwy/interpolation", test_interpolation);
     g_test_add_func("/testlibgwy/expr/evaluate", test_expr_evaluate);
     g_test_add_func("/testlibgwy/expr/vector", test_expr_vector);
