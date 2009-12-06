db644ef9a2ac5ae83a057eeb1ab79fd9d07901e0
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-12-06 21:59:24 +0000

    - corrected wrong definition of gwy_array_replace1() resolving to   gwy_array_insert()
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@10705 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/array.h
+++ branches/GWYDDION-3/libgwy/array.h
@@ -63,7 +63,7 @@ gpointer                gwy_array_get          (GwyArray *array,
 #define gwy_array_delete1(array,n) \
     gwy_array_delete((array),(n),1)
 #define gwy_array_replace1(array,n,item) \
-    gwy_array_insert((array),(n),(item),1)
+    gwy_array_replace((array),(n),(item),1)
 gpointer                gwy_array_insert       (GwyArray *array,
                                                 guint n,
                                                 gpointer items,
