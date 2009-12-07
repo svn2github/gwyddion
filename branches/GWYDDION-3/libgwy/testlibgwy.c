61c5f85532d5383f7ab2b7dc9db1566fd87b3a3c
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-12-07 16:23:10 +0000

    - added an Array test
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@10708 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/testlibgwy.c
+++ branches/GWYDDION-3/libgwy/testlibgwy.c
@@ -2272,7 +2272,7 @@ test_container_boxed(void)
 
 /***************************************************************************
  *
- * Inventory
+ * Array
  *
  ***************************************************************************/
 
@@ -2284,6 +2284,162 @@ record_item_change(G_GNUC_UNUSED GObject *object,
     *counter <<= 4;
     *counter |= pos + 1;
 }
+
+static void
+test_array_data(void)
+{
+    GwyRGBA init_colors[] = { { 1.0, 1.0, 1.0, 1.0 }, { 0.0, 0.0, 0.0, 0.0 } };
+    GwyArray *array = gwy_array_new_with_data(sizeof(GwyRGBA), NULL,
+                                              init_colors,
+                                              G_N_ELEMENTS(init_colors));
+    g_assert(GWY_IS_ARRAY(array));
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 2);
+    guint64 insert_log = 0, update_log = 0, delete_log = 0;
+    g_signal_connect(array, "item-inserted",
+                     G_CALLBACK(record_item_change), &insert_log);
+    g_signal_connect(array, "item-updated",
+                     G_CALLBACK(record_item_change), &update_log);
+    g_signal_connect(array, "item-deleted",
+                     G_CALLBACK(record_item_change), &delete_log);
+
+    // Single-item operations.
+    GwyRGBA color1 = { 0.1, 0.1, 0.1, 0.1 };
+    GwyRGBA color2 = { 0.2, 0.2, 0.2, 0.2 };
+    GwyRGBA color3 = { 0.3, 0.3, 0.3, 0.3 };
+    gwy_array_insert1(array, 1, &color1);
+    gwy_array_insert1(array, 3, &color2);
+    gwy_array_insert1(array, 0, &color3);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 5);
+    g_assert_cmphex(insert_log, ==, 0x241);
+    g_assert_cmphex(update_log, ==, 0);
+    g_assert_cmphex(delete_log, ==, 0);
+    insert_log = 0;
+
+    GwyRGBA *color;
+    color = gwy_array_get(array, 0);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color3, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 1);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, init_colors + 0, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 2);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color1, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 3);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, init_colors + 1, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 4);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color2, sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_delete1(array, 2);
+    gwy_array_delete1(array, 1);
+    gwy_array_delete1(array, 1);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 2);
+    g_assert_cmphex(insert_log, ==, 0);
+    g_assert_cmphex(update_log, ==, 0);
+    g_assert_cmphex(delete_log, ==, 0x322);
+    delete_log = 0;
+
+    color = gwy_array_get(array, 0);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color3, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 1);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color2, sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_append1(array, &color1);
+    gwy_array_replace1(array, 0, &color1);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 3);
+    g_assert_cmphex(insert_log, ==, 0x3);
+    g_assert_cmphex(update_log, ==, 0x1);
+    g_assert_cmphex(delete_log, ==, 0);
+    insert_log = update_log = 0;
+
+    color = gwy_array_get(array, 0);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color1, sizeof(GwyRGBA)), ==, 0);
+
+    // Multi-item operations.
+    GwyRGBA colors[] = { color1, color2, color3 };
+    gwy_array_insert(array, 1, init_colors, 2);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 5);
+    g_assert_cmphex(insert_log, ==, 0x23);
+    g_assert_cmphex(update_log, ==, 0);
+    g_assert_cmphex(delete_log, ==, 0);
+    insert_log = 0;
+
+    color = gwy_array_get(array, 1);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, init_colors + 0, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 2);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, init_colors + 1, sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_replace(array, 2, colors, 3);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 5);
+    g_assert_cmphex(insert_log, ==, 0);
+    g_assert_cmphex(update_log, ==, 0x345);
+    g_assert_cmphex(delete_log, ==, 0);
+    update_log = 0;
+
+    color = gwy_array_get(array, 2);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color1, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 3);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color2, sizeof(GwyRGBA)), ==, 0);
+
+    color = gwy_array_get(array, 4);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color3, sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_delete(array, 1, 3);
+    g_assert_cmpuint(gwy_array_n_items(array), ==, 2);
+    g_assert_cmphex(insert_log, ==, 0);
+    g_assert_cmphex(update_log, ==, 0);
+    g_assert_cmphex(delete_log, ==, 0x432);
+    delete_log = 0;
+
+    color = gwy_array_get(array, 1);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, &color3, sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_set_data(array, colors, 3);
+    g_assert_cmphex(insert_log, ==, 0x3);
+    g_assert_cmphex(update_log, ==, 0x12);
+    g_assert_cmphex(delete_log, ==, 0);
+    insert_log = update_log = 0;
+
+    color = gwy_array_get_data(array);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, colors, 3*sizeof(GwyRGBA)), ==, 0);
+
+    gwy_array_set_data(array, init_colors, 2);
+    g_assert_cmphex(insert_log, ==, 0);
+    g_assert_cmphex(update_log, ==, 0x12);
+    g_assert_cmphex(delete_log, ==, 0x3);
+    update_log = delete_log = 0;
+
+    color = gwy_array_get_data(array);
+    g_assert(color);
+    g_assert_cmpint(memcmp(color, init_colors, 2*sizeof(GwyRGBA)), ==, 0);
+
+    g_object_unref(array);
+}
+
+/***************************************************************************
+ *
+ * Inventory
+ *
+ ***************************************************************************/
 
 typedef struct {
     gchar *name;
@@ -2369,6 +2525,7 @@ test_inventory_data(void)
     };
 
     GwyInventory *inventory = gwy_inventory_new();
+    g_assert(GWY_IS_INVENTORY(inventory));
     gwy_inventory_set_item_type(inventory, &item_type);
     item_destroy_count = 0;
 
@@ -2712,6 +2869,7 @@ main(int argc, char *argv[])
     g_test_add_func("/testlibgwy/container/serialize", test_container_serialize);
     g_test_add_func("/testlibgwy/container/text", test_container_text);
     g_test_add_func("/testlibgwy/container/boxed", test_container_boxed);
+    g_test_add_func("/testlibgwy/array/data", test_array_data);
     g_test_add_func("/testlibgwy/inventory/data", test_inventory_data);
 
     return g_test_run();
