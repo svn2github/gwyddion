35877181a07f618ed4d74f3ae22c1e334418f7cf
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-01-15 10:06:21 +0000

    - added a test for linear function preservation also for non-interpolating   bases
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11002 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/interpolation.c
+++ branches/GWYDDION-3/libgwy/tests/interpolation.c
@@ -72,6 +72,37 @@ test_interpolation_linear(GwyInterpolationType interpolation,
     }
 }
 
+static void
+test_interpolation_linear_long(GwyInterpolationType interpolation,
+                               gdouble a,
+                               gdouble b)
+{
+    guint support_len = gwy_interpolation_get_support_size(interpolation);
+    g_assert_cmpint(support_len, >, 0);
+    /* This is just an implementation assertion, not consistency check. */
+    g_assert_cmpint(support_len % 2, ==, 0);
+    // Make the linear segment long enough even for non-interpolating
+    // basis functions.
+    guint len = 50*support_len;
+
+    gdouble data[len];
+    for (guint i = 0; i < len; i++) {
+        gdouble origin = len/2 - support_len/2 - 1.0;
+        data[i] = (i - origin)*(b - a) + a;
+    }
+
+    gwy_interpolation_resolve_coeffs_1d(len, data, interpolation);
+
+    for (gdouble x = 0.0; x < 1.0; x += 0.0618) {
+        gdouble value = x*b + (1.0 - x)*a;
+        gdouble ivalue = gwy_interpolate_1d(x, data + len/2 - support_len,
+                                            interpolation);
+        g_assert_cmpfloat(fabs(ivalue - value),
+                          <=,
+                          1e-14 * (fabs(ivalue) + fabs(value)));
+    }
+}
+
 void
 test_interpolation(void)
 {
@@ -84,16 +115,26 @@ test_interpolation(void)
         test_interpolation_constant(interp, -1.0);
     }
 
-    /* FIXME: The interpolations with non-interpolating basis reproduce
-     * the linear function only on infinite interval.  How to check them? */
-    GwyInterpolationType reproducing_linear[] = {
+    /* Interpolating basis. */
+    GwyInterpolationType reproducing_linear1[] = {
         GWY_INTERPOLATION_LINEAR,
         GWY_INTERPOLATION_KEYS,
         GWY_INTERPOLATION_SCHAUM,
     };
-    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear); i++) {
-        test_interpolation_linear(reproducing_linear[i], 1.0, 2.0);
-        test_interpolation_linear(reproducing_linear[i], 1.0, -1.0);
+    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear1); i++) {
+        test_interpolation_linear(reproducing_linear1[i], 1.0, 2.0);
+        test_interpolation_linear(reproducing_linear1[i], 1.0, -1.0);
+    }
+
+    /* Non-interpolating basis. */
+    GwyInterpolationType reproducing_linear2[] = {
+        GWY_INTERPOLATION_BSPLINE4,
+        GWY_INTERPOLATION_OMOMS4,
+        GWY_INTERPOLATION_BSPLINE6,
+    };
+    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear2); i++) {
+        test_interpolation_linear_long(reproducing_linear2[i], 1.0, 2.0);
+        test_interpolation_linear_long(reproducing_linear2[i], 1.0, -1.0);
     }
 }
 
