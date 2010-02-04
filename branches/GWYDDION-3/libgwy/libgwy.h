4789ca2b20140414ed36a7f2b79ce769e6885b8f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-02-04 23:42:01 +0000

    - added missing #include of mask-field-transform.h
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11096 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/libgwy.h
+++ branches/GWYDDION-3/libgwy/libgwy.h
@@ -47,6 +47,7 @@
 #include <libgwy/macros.h>
 #include <libgwy/main.h>
 #include <libgwy/mask-field.h>
+#include <libgwy/mask-field-transform.h>
 #include <libgwy/math.h>
 #include <libgwy/pack.h>
 #include <libgwy/resource.h>
