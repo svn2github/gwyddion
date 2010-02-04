ebbfdf4ff50edd01db9c7087b770f1edc998bc34
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-02-04 22:48:40 +0000

    - added a test for gwy_field_statistics(), actually checks only the values of   mean and rms
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11095 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/testlibgwy.c
+++ branches/GWYDDION-3/libgwy/tests/testlibgwy.c
@@ -93,6 +93,7 @@ void test_field_set_size       (void);
 void test_field_range          (void);
 void test_field_mean           (void);
 void test_field_rms            (void);
+void test_field_statistics     (void);
 void test_field_surface_area   (void);
 void test_container_data       (void);
 void test_container_refcount   (void);
@@ -221,6 +222,7 @@ main(int argc, char *argv[])
     g_test_add_func("/testlibgwy/field/range", test_field_range);
     g_test_add_func("/testlibgwy/field/mean", test_field_mean);
     g_test_add_func("/testlibgwy/field/rms", test_field_rms);
+    g_test_add_func("/testlibgwy/field/statistics", test_field_statistics);
     g_test_add_func("/testlibgwy/field/surface-area", test_field_surface_area);
     g_test_add_func("/testlibgwy/container/data", test_container_data);
     g_test_add_func("/testlibgwy/container/refcount", test_container_refcount);
