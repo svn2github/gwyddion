fdfa4a34bc78387b63daa3332a1dc879b0e6022f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-02-05 10:23:00 +0000

    - improved get make var
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11104 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/build/get-make-var.py
+++ branches/GWYDDION-3/build/get-make-var.py
@@ -2,19 +2,29 @@
 import sys, re
 from subprocess import Popen, PIPE
 
-vars = {}
+variables = {}
 refre = re.compile(r'(?<!\$)\$(\{\w+\}|\(\w+\))').sub
 varre = re.compile(r'(?P<name>\w+)\s*=\s*(?P<value>.*)').match
 
 def expand(match):
     name = match.group(1)[1:-1]
-    return refre(expand, vars[name])
+    if name not in variables:
+        return ''
+    return refre(expand, variables[name])
 
 output = Popen(['gmake', '-qp'], stdout=PIPE).communicate()[0]
 for line in output.split('\n'):
     m = varre(line)
     if m:
-        vars[m.group('name')] = m.group('value').strip()
+        variables[m.group('name')] = m.group('value').strip()
+
+bare = False
+if len(sys.argv) > 1 and sys.argv[1] in ('-b', '--bare'):
+    bare = True
+    del sys.argv[1]
 
 for name in sys.argv[1:]:
-    print '%s = %s' % (name, refre(expand, '$(' + name + ')'))
+    if bare:
+        print refre(expand, '$(%s)' % name)
+    else:
+        print '%s = %s' % (name, refre(expand, '$(%s)' % name))
