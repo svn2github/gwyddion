dee86aab00fa0da04e05e0ba6b01cfc443bea5f4
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-02-06 22:52:37 +0000

    - added UserFitFunc saving and serialization tests
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11126 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/user-fit-func.c
+++ branches/GWYDDION-3/libgwy/tests/user-fit-func.c
@@ -27,6 +27,39 @@
  *
  ***************************************************************************/
 
+/* XXX: GwyFitParam is not serializable.  Make it so?
+void
+test_fit_param_boxed(void)
+{
+    GwyFitParam fitparam = { (gchar*)"b0", (gchar*)"xmid", 1, 0 };
+    GwyFitParam *copy = serialize_boxed_and_back(&fitparam, GWY_TYPE_FIT_PARAM);
+    g_assert_cmpstr(fitparam.name, ==, copy->name);
+    g_assert_cmpint(fitparam.power_x, ==, copy->power_x);
+    g_assert_cmpint(fitparam.power_y, ==, copy->power_y);
+    g_assert_cmpstr(fitparam.estimate, ==, copy->estimate);
+    g_boxed_free(GWY_TYPE_FIT_PARAM, copy);
+}
+*/
+
+static void
+test_user_fit_func_param_check(GwyUserFitFunc *userfitfunc,
+                               guint expected_n_params,
+                               const GwyFitParam *expected_params)
+{
+    g_assert_cmpuint(gwy_user_fit_func_n_params(userfitfunc),
+                     ==, expected_n_params);
+    for (guint i = 0; i < expected_n_params; i++) {
+        const GwyFitParam *expected = expected_params + i;
+        const GwyFitParam *param = gwy_user_fit_func_param(userfitfunc,
+                                                           expected->name);
+        g_assert(param);
+        g_assert_cmpstr(param->name, ==, expected->name);
+        g_assert_cmpstr(param->estimate, ==, expected->estimate);
+        g_assert_cmpint(param->power_x, ==, expected->power_x);
+        g_assert_cmpint(param->power_y, ==, expected->power_y);
+    }
+}
+
 static void
 test_user_fit_func_load_check(const gchar *filename,
                               const gchar *expected_name,
@@ -55,18 +88,8 @@ test_user_fit_func_load_check(const gchar *filename,
 
     g_assert_cmpstr(gwy_user_fit_func_get_formula(userfitfunc),
                     ==, expected_formula);
-    g_assert_cmpuint(gwy_user_fit_func_n_params(userfitfunc),
-                     ==, expected_n_params);
-    for (guint i = 0; i < expected_n_params; i++) {
-        const GwyFitParam *expected = expected_params + i;
-        const GwyFitParam *param = gwy_user_fit_func_param(userfitfunc,
-                                                           expected->name);
-        g_assert(param);
-        g_assert_cmpstr(param->name, ==, expected->name);
-        g_assert_cmpstr(param->estimate, ==, expected->estimate);
-        g_assert_cmpint(param->power_x, ==, expected->power_x);
-        g_assert_cmpint(param->power_y, ==, expected->power_y);
-    }
+    test_user_fit_func_param_check(userfitfunc,
+                                   expected_n_params, expected_params);
     GWY_OBJECT_UNREF(userfitfunc);
 }
 
@@ -138,37 +161,34 @@ test_user_fit_func_load(void)
                                   "a+b*x", 2, params);
 }
 
-#if 0
 void
 test_user_fit_func_save(void)
 {
-    static const GwyUserFitFuncPoint gradient_point_red0 = { 0, { 0.8, 0, 0, 1 } };
-    static const GwyUserFitFuncPoint gradient_point_hg = { 0.5, { 0, 0.6, 0, 1 } };
-    static const GwyUserFitFuncPoint gradient_point_blue1 = { 1, { 0, 0, 1, 1 } };
+    static const GwyFitParam param_a = { "a", "ymean", 0, 1 };
+    static const GwyFitParam param_b = { "b", "(yxmax-yxmin)/(xmax-xmin)", -1, 1 };
 
+    GwyFitParam params[] = { param_a, param_b };
     GwyUserFitFunc *userfitfunc = gwy_user_fit_func_new();
     GwyResource *resource = GWY_RESOURCE(userfitfunc);
 
-    gwy_resource_set_name(resource, "Tricolor");
-    g_assert_cmpstr(gwy_resource_get_name(resource), ==, "Tricolor");
+    gwy_resource_set_name(resource, "Linear 2");
+    g_assert_cmpstr(gwy_resource_get_name(resource), ==, "Linear 2");
 
-    gwy_user_fit_func_set_color(userfitfunc, 0, &gradient_point_red0.color);
-    gwy_user_fit_func_set_color(userfitfunc, 1, &gradient_point_blue1.color);
-    gwy_user_fit_func_insert_sorted(userfitfunc, &gradient_point_hg);
-    g_assert_cmpuint(gwy_user_fit_func_n_points(userfitfunc), ==, 3);
-    GwyUserFitFuncPoint pt = gwy_user_fit_func_get(userfitfunc, 1);
-    g_assert_cmpint(memcmp(&pt, &gradient_point_hg, sizeof(GwyUserFitFuncPoint)),
-                    ==, 0);
+    g_assert(gwy_user_fit_func_set_formula(userfitfunc, "a+b*x", NULL));
+    g_assert_cmpstr(gwy_user_fit_func_get_formula(userfitfunc), ==, "a+b*x");
+    g_assert_cmpuint(gwy_user_fit_func_n_params(userfitfunc), ==, 2);
+    gwy_user_fit_func_update_param(userfitfunc, &param_a);
+    gwy_user_fit_func_update_param(userfitfunc, &param_b);
 
     GError *error = NULL;
-    gwy_resource_set_filename(resource, "Tricolor");
+    gwy_resource_set_filename(resource, "Linear2");
     g_assert(gwy_resource_save(resource, &error));
     g_assert(!error);
-    g_test_queue_destroy((GDestroyNotify)g_unlink, "Tricolor");
+    g_test_queue_destroy((GDestroyNotify)g_unlink, "Linear2");
 
     gchar *res_filename = NULL;
     g_object_get(resource, "file-name", &res_filename, NULL);
-    GFile *gfile = g_file_new_for_path("Tricolor");
+    GFile *gfile = g_file_new_for_path("Linear2");
     GFile *res_gfile = g_file_new_for_path(res_filename);
     g_assert(g_file_equal(gfile, res_gfile));
     g_object_unref(gfile);
@@ -177,63 +197,57 @@ test_user_fit_func_save(void)
 
     GWY_OBJECT_UNREF(userfitfunc);
 
-    test_user_fit_func_load_check("Tricolor", "Tricolor", 3,
-                             &gradient_point_red0, &gradient_point_blue1);
+    test_user_fit_func_load_check("Linear2", "Linear 2",
+                                  "a+b*x", 2, params);
 }
 
 void
 test_user_fit_func_serialize(void)
 {
-    static const GwyUserFitFuncPoint gradient_point_red0 = { 0, { 0.8, 0, 0, 1 } };
-    static const GwyUserFitFuncPoint gradient_point_blue1 = { 1, { 0, 0, 1, 1 } };
+    static const GwyFitParam param_a = { "a", "ymean", 0, 1 };
+    static const GwyFitParam param_b = { "b", "(yxmax-yxmin)/(xmax-xmin)", -1, 1 };
+
+    GwyFitParam params[] = { param_a, param_b };
 
     GwyUserFitFunc *userfitfunc = gwy_user_fit_func_new();
     GwyResource *resource = GWY_RESOURCE(userfitfunc);
 
-    gwy_resource_set_name(resource, "Red-Blue");
-    g_assert_cmpstr(gwy_resource_get_name(resource), ==, "Red-Blue");
+    gwy_resource_set_name(resource, "Linear-3");
+    g_assert_cmpstr(gwy_resource_get_name(resource), ==, "Linear-3");
 
-    GwyUserFitFuncPoint pt;
-    gwy_user_fit_func_set(userfitfunc, 0, &gradient_point_red0);
-    gwy_user_fit_func_set(userfitfunc, 1, &gradient_point_blue1);
-    pt = gwy_user_fit_func_get(userfitfunc, 0);
-    g_assert_cmpint(memcmp(&pt, &gradient_point_red0,
-                           sizeof(GwyUserFitFuncPoint)), ==, 0);
-    pt = gwy_user_fit_func_get(userfitfunc, 1);
-    g_assert_cmpint(memcmp(&pt, &gradient_point_blue1,
-                           sizeof(GwyUserFitFuncPoint)), ==, 0);
+    g_assert(gwy_user_fit_func_set_formula(userfitfunc, "a+b*x", NULL));
+    g_assert_cmpstr(gwy_user_fit_func_get_formula(userfitfunc), ==, "a+b*x");
+    g_assert_cmpuint(gwy_user_fit_func_n_params(userfitfunc), ==, 2);
+    gwy_user_fit_func_update_param(userfitfunc, &param_a);
+    gwy_user_fit_func_update_param(userfitfunc, &param_b);
 
-    GwyUserFitFunc *newgradient
+    GwyUserFitFunc *newuserfitfunc
         = (GwyUserFitFunc*)serialize_and_back(G_OBJECT(userfitfunc));
-    GwyResource *newresource = GWY_RESOURCE(newgradient);
-    g_assert_cmpstr(gwy_resource_get_name(newresource), ==, "Red-Blue");
-
-    pt = gwy_user_fit_func_get(newgradient, 0);
-    g_assert_cmpint(memcmp(&pt, &gradient_point_red0,
-                           sizeof(GwyUserFitFuncPoint)), ==, 0);
-    pt = gwy_user_fit_func_get(newgradient, 1);
-    g_assert_cmpint(memcmp(&pt, &gradient_point_blue1,
-                           sizeof(GwyUserFitFuncPoint)), ==, 0);
+    GwyResource *newresource = GWY_RESOURCE(newuserfitfunc);
+    g_assert_cmpstr(gwy_resource_get_name(newresource), ==, "Linear-3");
+    g_assert_cmpstr(gwy_user_fit_func_get_formula(userfitfunc), ==, "a+b*x");
+    test_user_fit_func_param_check(newuserfitfunc, 2, params);
 
     g_object_unref(userfitfunc);
-    g_object_unref(newgradient);
+    g_object_unref(newuserfitfunc);
 }
 
+#if 0
 void
 test_user_fit_func_inventory(void)
 {
-    static const GwyUserFitFuncPoint gradient_point_red = { 0.5, { 1, 0, 0, 1 } };
+    static const GwyUserFitFuncPoint userfitfunc_point_red = { 0.5, { 1, 0, 0, 1 } };
     const GwyInventoryItemType *item_type;
     GwyUserFitFunc *userfitfunc;
     GwyResource *resource;
 
-    GwyInventory *gradients = gwy_user_fit_funcs();
-    g_assert(GWY_IS_INVENTORY(gradients));
-    item_type = gwy_inventory_get_item_type(gradients);
+    GwyInventory *userfitfuncs = gwy_user_fit_funcs();
+    g_assert(GWY_IS_INVENTORY(userfitfuncs));
+    item_type = gwy_inventory_get_item_type(userfitfuncs);
     g_assert(item_type);
     g_assert_cmpuint(item_type->type, ==, GWY_TYPE_USER_FIT_FUNC);
-    g_assert(gwy_inventory_can_make_copies(gradients));
-    g_assert_cmpstr(gwy_inventory_get_default_name(gradients),
+    g_assert(gwy_inventory_can_make_copies(userfitfuncs));
+    g_assert_cmpstr(gwy_inventory_get_default_name(userfitfuncs),
                     ==, GWY_USER_FIT_FUNC_DEFAULT);
 
     item_type = gwy_resource_type_get_item_type(GWY_TYPE_USER_FIT_FUNC);
@@ -246,7 +260,7 @@ test_user_fit_func_inventory(void)
     g_assert(gwy_resource_is_managed(resource));
     g_assert(!gwy_resource_is_modifiable(resource));
 
-    userfitfunc = gwy_inventory_get_default(gradients);
+    userfitfunc = gwy_inventory_get_default(userfitfuncs);
     g_assert(GWY_IS_USER_FIT_FUNC(userfitfunc));
     resource = GWY_RESOURCE(userfitfunc);
     g_assert_cmpstr(gwy_resource_get_name(resource), ==, GWY_USER_FIT_FUNC_DEFAULT);
@@ -257,9 +271,9 @@ test_user_fit_func_inventory(void)
     gwy_resource_set_is_preferred(resource, TRUE);
     g_assert(gwy_resource_get_is_preferred(resource));
 
-    gwy_inventory_copy(gradients, GWY_USER_FIT_FUNC_DEFAULT, "Another");
-    g_assert_cmpuint(gwy_inventory_n_items(gradients), ==, 2);
-    userfitfunc = gwy_inventory_get(gradients, "Another");
+    gwy_inventory_copy(userfitfuncs, GWY_USER_FIT_FUNC_DEFAULT, "Another");
+    g_assert_cmpuint(gwy_inventory_n_items(userfitfuncs), ==, 2);
+    userfitfunc = gwy_inventory_get(userfitfuncs, "Another");
     resource = GWY_RESOURCE(userfitfunc);
     g_assert_cmpstr(gwy_resource_get_name(resource), ==, "Another");
     gboolean is_modified;
@@ -267,11 +281,11 @@ test_user_fit_func_inventory(void)
     g_assert(gwy_resource_is_managed(resource));
     g_assert(gwy_resource_is_modifiable(resource));
     g_assert(is_modified);
-    gwy_user_fit_func_insert_sorted(userfitfunc, &gradient_point_red);
+    gwy_user_fit_func_insert_sorted(userfitfunc, &userfitfunc_point_red);
     g_assert_cmpuint(gwy_user_fit_func_n_points(userfitfunc), ==, 3);
 
     g_object_ref(userfitfunc);
-    gwy_inventory_delete(gradients, "Another");
+    gwy_inventory_delete(userfitfuncs, "Another");
     g_assert(GWY_IS_USER_FIT_FUNC(userfitfunc));
     g_assert(!gwy_resource_is_managed(resource));
     g_assert(gwy_resource_is_modifiable(resource));
