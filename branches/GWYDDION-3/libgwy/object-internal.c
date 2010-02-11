22c14f378a1066fe14164599aeade0e48b8d37b5
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2010-02-11 09:34:16 +0000

    - corrected aliasing macro name
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11177 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/object-internal.c
+++ branches/GWYDDION-3/libgwy/object-internal.c
@@ -134,7 +134,7 @@ _gwy_check_object_component(const GwySerializableItem *item,
     g_return_val_if_reached(FALSE);
 }
 
-#define __LIBGWY_OBJECT_C__
+#define __LIBGWY_OBJECT_INTERNAL_C__
 #include "libgwy/libgwy-aliases.c"
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
