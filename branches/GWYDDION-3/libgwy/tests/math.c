7cc142bb69302296ed51d6fa6ed8c9f3e5927f9a
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-01-25 21:49:49 +0000

    - added a gwy_powi() test
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11869 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/libgwy/tests/math.c
@@ -0,0 +1,63 @@
+/*
+ *  $Id$
+ *  Copyright (C) 2009 David Necas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "testlibgwy.h"
+
+/***************************************************************************
+ *
+ * Basic math functions
+ *
+ ***************************************************************************/
+
+void
+test_math_powi_actual(void)
+{
+    g_assert_cmpfloat(gwy_powi(0.0, 0), ==, 1.0);
+
+    long double x = -G_PI;
+    long double pp = 1.0, pm = 1.0;
+    for (gint i = 0; i < 40; i++) {
+        g_assert_cmpfloat(fabs(gwy_powi(x, i) - pp), <=, 5e-16*fabs(pp));
+        g_assert_cmpfloat(fabs(gwy_powi(x, -i) - pm), <=, 5e-16*fabs(pm));
+        pp *= x;
+        pm /= x;
+    }
+}
+
+// Ensure we get the Gwyddion implementation
+#undef gwy_powi
+double gwy_powi(double x, int i);
+
+void
+test_math_powi_our(void)
+{
+    g_assert_cmpfloat(gwy_powi(0.0, 0), ==, 1.0);
+
+    long double x = -G_PI;
+    long double pp = 1.0, pm = 1.0;
+    double arg = x;
+    for (gint i = 0; i < 40; i++) {
+        g_assert_cmpfloat(fabs(gwy_powi(arg, i) - pp), <=, 5e-16*fabs(pp));
+        g_assert_cmpfloat(fabs(gwy_powi(arg, -i) - pm), <=, 5e-16*fabs(pm));
+        pp *= x;
+        pm /= x;
+    }
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
