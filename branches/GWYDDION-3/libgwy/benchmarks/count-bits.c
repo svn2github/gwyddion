5f4871dc39d5869fa51713d368ff72fdf9773882
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-02-04 09:50:56 +0000

    - moved bit-counting benchmark from tests to benchmarks
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@11969 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/libgwy/benchmarks/count-bits.c
@@ -0,0 +1,360 @@
+/*
+ *  $Id$
+ *  Copyright (C) 2009-2011 David Necas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
+ *
+ *  The quicksort algorithm was copied from GNU C library,
+ *  Copyright (C) 1991, 1992, 1996, 1997, 1999 Free Software Foundation, Inc.
+ *  See below.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <glib.h>
+#include "timer.h"
+
+#ifdef __GNUC__
+#define count_set_bits_gcc __builtin_popcount
+#endif
+
+static inline guint
+count_set_bits_l1(guint32 x)
+{
+    guint count = 0;
+    while (x) {
+        if (x & 1)
+            count++;
+        x >>= 1;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l1c(guint32 x)
+{
+    guint count = 0;
+    while (x) {
+        count += x & 1;
+        x >>= 1;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l2c(guint32 x)
+{
+    static const guint8 table[4] = { 0, 1, 1, 2 };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0x3];
+        x >>= 2;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l4c(guint32 x)
+{
+    static const guint8 table[16] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xf];
+        x >>= 4;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l8c(guint32 x)
+{
+    static const guint8 table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xff];
+        x >>= 8;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l2h(guint32 x)
+{
+    static const guint16 table[4] = { 0, 1, 1, 2 };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0x3];
+        x >>= 2;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l4h(guint32 x)
+{
+    static const guint16 table[16] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xf];
+        x >>= 4;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l8h(guint32 x)
+{
+    static const guint16 table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xff];
+        x >>= 8;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_lu8h(guint32 x)
+{
+    static const guint16 table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    guint count = 0;
+    if (!x)
+        return count;
+    count += table[x & 0xff];
+    x >>= 8;
+    if (!x)
+        return count;
+    count += table[x & 0xff];
+    x >>= 8;
+    if (!x)
+        return count;
+    count += table[x & 0xff];
+    x >>= 8;
+    if (!x)
+        return count;
+    count += table[x & 0xff];
+    return count;
+}
+
+static inline guint
+count_set_bits_u8h(guint32 x)
+{
+    static const guint16 table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    guint count = 0;
+    count += table[x & 0xff];
+    x >>= 8;
+    count += table[x & 0xff];
+    x >>= 8;
+    count += table[x & 0xff];
+    x >>= 8;
+    count += table[x & 0xff];
+    return count;
+}
+
+static inline guint
+count_set_bits_l2w(guint32 x)
+{
+    static const guint table[4] = { 0, 1, 1, 2 };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0x3];
+        x >>= 2;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l4w(guint32 x)
+{
+    static const guint table[16] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xf];
+        x >>= 4;
+    }
+    return count;
+}
+
+static inline guint
+count_set_bits_l8w(guint32 x)
+{
+    static const guint table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    guint count = 0;
+    while (x) {
+        count += table[x & 0xff];
+        x >>= 8;
+    }
+    return count;
+}
+
+#define COUNT_BITS_LOOP(name) \
+    do { \
+        gwy_benchmark_timer_start(); \
+        guint count = 0; \
+        for (guint iter = 0; iter < niter; iter++) { \
+            guint32 *p = pool; \
+            for (guint i = size; i; i--) \
+                count += count_set_bits_##name(*(p++)); \
+        } \
+        gwy_benchmark_timer_stop(); \
+        printf("%s: time=%g, count=%u\n", \
+               #name, gwy_benchmark_timer_get_user(), count); \
+    } while (0)
+
+int
+main(int argc, char *argv[])
+{
+    guint size = 65536, niter = 2000,
+          rand_seed = 42;
+
+    GOptionEntry entries[] = {
+        { "size",        's', 0, G_OPTION_ARG_INT, &size,      "Array size to test (in bytes)", "N",  },
+        { "n-iter",      'n', 0, G_OPTION_ARG_INT, &niter,     "Number of iterations",          "N",  },
+        { "random-seed", 'r', 0, G_OPTION_ARG_INT, &rand_seed, "Random seed",                   "R",  },
+        { NULL,          0,   0, 0,                NULL,       NULL,                            NULL, },
+    };
+
+    GError *error = NULL;
+    GOptionContext *context = g_option_context_new("- optimise counting of set bits");
+    g_option_context_add_main_entries(context, entries, NULL);
+    if (!g_option_context_parse(context, &argc, &argv, &error)) {
+        g_printerr("Arguments parsing failed: %s\n", error->message);
+        return 1;
+    }
+    g_option_context_free(context);
+    setvbuf(stdout, (char*)NULL, _IOLBF, 0);
+
+    GRand *rng = g_rand_new();
+    g_rand_set_seed(rng, rand_seed);
+
+    guint32 *pool = g_new(guint32, size);
+    for (guint i = 0; i < size; i++)
+        pool[i] = g_rand_int(rng);
+
+    COUNT_BITS_LOOP(l1);
+    COUNT_BITS_LOOP(l1c);
+    COUNT_BITS_LOOP(l2c);
+    COUNT_BITS_LOOP(l2h);
+    COUNT_BITS_LOOP(l2w);
+    COUNT_BITS_LOOP(l4c);
+    COUNT_BITS_LOOP(l4h);
+    COUNT_BITS_LOOP(l4w);
+    COUNT_BITS_LOOP(l8c);
+    COUNT_BITS_LOOP(l8h);
+    COUNT_BITS_LOOP(lu8h);
+    COUNT_BITS_LOOP(u8h);
+    COUNT_BITS_LOOP(l8w);
+#ifdef __GNUC__
+    COUNT_BITS_LOOP(gcc);
+#endif
+
+    g_free(pool);
+    g_rand_free(rng);
+
+    return 0;
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
--- branches/GWYDDION-3/libgwy/benchmarks/timer.h
+++ branches/GWYDDION-3/libgwy/benchmarks/timer.h
@@ -58,3 +58,5 @@ gwy_benchmark_timer_get_wall(void)
 }
 
 #endif
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
