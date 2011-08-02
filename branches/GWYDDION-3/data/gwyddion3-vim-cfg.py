d1e6b7157257b02463420e0a2400632078f3d0f2
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-08-02 07:03:55 +0000

    - updated gwyddion.vim → gwyddion3.vim in more places
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@12657 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/data/gwyddion3-vim-cfg.py
+++ branches/GWYDDION-3/data/gwyddion3-vim-cfg.py
@@ -1,5 +1,5 @@
 from os import getcwd as _getcwd
 file_glob = _getcwd() + '/../docs/*/*-decl.txt'
 maintainer = 'Yeti <yeti@gwyddion.net>'
-syntax_name = 'gwyddion'
-url = 'http://gwyddion.net/download/gwyddion.vim'
+syntax_name = 'gwyddion3'
+url = 'http://gwyddion.net/download/gwyddion3.vim'
