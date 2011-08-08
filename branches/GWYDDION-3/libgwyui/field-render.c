34eb62025183fe83330d0d4622d5df535e2a06e9
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-08-08 14:49:00 +0000

    - initial implementation of gwy_field_render()
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@12705 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwyui/field-render.c
+++ branches/GWYDDION-3/libgwyui/field-render.c
@@ -19,6 +19,118 @@
 
 #include "libgwyui/field-render.h"
 
+#define NOT_QUITE_1 0.999999999999999
+
+#define COMPONENT_TO_PIXEL8(x) (guint)(256*CLAMP((x), 0.0, NOT_QUITE_1))
+
+typedef struct {
+    guint prev, next;
+    gdouble wprev, wnext;
+} InterpolationPoint;
+
+static inline void
+gwy_rgba_to_rgb8_pixel(const GwyRGBA *rgba,
+                       guchar *pixel)
+{
+    *(pixel++) = COMPONENT_TO_PIXEL8(rgba->r);
+    *(pixel++) = COMPONENT_TO_PIXEL8(rgba->g);
+    *pixel = COMPONENT_TO_PIXEL8(rgba->b);
+}
+
+static inline void
+gwy_rgba_to_rgba8_pixel(const GwyRGBA *rgba,
+                        guchar *pixel)
+{
+    *(pixel++) = COMPONENT_TO_PIXEL8(rgba->r);
+    *(pixel++) = COMPONENT_TO_PIXEL8(rgba->g);
+    *(pixel++) = COMPONENT_TO_PIXEL8(rgba->b);
+    *pixel = COMPONENT_TO_PIXEL8(rgba->a);
+}
+
+static inline guint32
+gwy_rgba_to_rgb8_color(const GwyRGBA *rgba)
+{
+    guint red = COMPONENT_TO_PIXEL8(rgba->r),
+          green = COMPONENT_TO_PIXEL8(rgba->g),
+          blue = COMPONENT_TO_PIXEL8(rgba->b);
+    return 0xffu | (blue << 8) | (green << 16) | (red << 24);
+}
+
+static inline guint32
+gwy_rgba_to_rgba8_color(const GwyRGBA *rgba)
+{
+    guint red = COMPONENT_TO_PIXEL8(rgba->r),
+          green = COMPONENT_TO_PIXEL8(rgba->g),
+          blue = COMPONENT_TO_PIXEL8(rgba->b),
+          alpha = COMPONENT_TO_PIXEL8(rgba->a);
+    return alpha | (blue << 8) | (green << 16) | (red << 24);
+}
+
+static InterpolationPoint*
+build_interpolation(guint n, gdouble from, gdouble to, guint res)
+{
+    InterpolationPoint *intpoints = g_new(InterpolationPoint, n);
+    gdouble q = (to - from)/n;
+    gdouble r = 0.5*MIN(q, NOT_QUITE_1);
+    gdouble limit = NOT_QUITE_1*res;
+
+    for (guint i = 0; i < n; i++) {
+        InterpolationPoint *ip = intpoints + i;
+        gdouble centre = q*(i + 0.5) + from;
+        gdouble left = centre - r, right = centre + r;
+
+        left = CLAMP(left, 0.0, limit);
+        right = CLAMP(right, 0.0, limit);
+
+        ip->prev = (guint)left;
+        ip->next = (guint)right;
+        g_assert(ip->prev < res);
+        g_assert(ip->next < res);
+
+        if (ip->next == ip->prev)
+            ip->wprev = ip->wnext = 0.5;
+        else {
+            g_assert(ip->prev + 1 == ip->next);
+            ip->wprev = left - ip->prev;
+            ip->wnext = 1.0 - ip->wprev;
+            g_assert(ip->wprev >= 0.0);
+            g_assert(ip->wnext >= 0.0);
+        }
+    }
+
+    return intpoints;
+}
+
+static void
+field_render_empty_range(GdkPixbuf *pixbuf,
+                         GwyGradient *gradient)
+{
+    GwyRGBA rgba;
+    gwy_gradient_color(gradient, 0.5, &rgba);
+    guint32 color = gwy_rgba_to_rgb8_color(&rgba);
+    gdk_pixbuf_fill(pixbuf, color);
+}
+
+/**
+ * gwy_field_render:
+ * @field: A two-dimensional data field.
+ * @pixbuf: A pixbuf.
+ * @gradient: A false colour gradient.
+ * @xfrom: Horizontal coordinate of the left edge of the area.
+ * @yfrom: Vertical coordinate of the upper edge of the area.
+ * @xto: Horizontal coordinate of the right edge of the area.
+ * @yto: Vertical coordinate of the lower edge of the area.
+ * @min: Value to map to @gradient begining.
+ * @max: Value to map to @gradient end.
+ *
+ * Renders a field to pixbuf using false colour gradient.
+ *
+ * Parameters defining the area to render are measured in pixels; the entire
+ * area must line within @field.
+ *
+ * The value range, determined by @min and @max, can be arbitrary.  Passing
+ * @max smaller than @min renders the gradient inversely.
+ **/
 void
 gwy_field_render(const GwyField *field,
                  GdkPixbuf *pixbuf,
@@ -30,9 +142,54 @@ gwy_field_render(const GwyField *field,
     g_return_if_fail(GWY_IS_FIELD(field));
     g_return_if_fail(GDK_IS_PIXBUF(pixbuf));
     g_return_if_fail(GWY_IS_GRADIENT(gradient));
+    g_return_if_fail(xfrom >= 0.0 && xfrom <= xto && xto <= field->xres);
+    g_return_if_fail(yfrom >= 0.0 && yfrom <= yto && yto <= field->yres);
 
-    if (min == max)
-        max = G_MAXDOUBLE;
+    if (min == max) {
+        field_render_empty_range(pixbuf, gradient);
+        return;
+    }
+
+    guint width = gdk_pixbuf_get_width(pixbuf);
+    guint height = gdk_pixbuf_get_width(pixbuf);
+    guint rowstride = gdk_pixbuf_get_rowstride(pixbuf);
+    guchar *pixels = gdk_pixbuf_get_pixels(pixbuf);
+
+    guint xres = field->xres, yres = field->yres;
+    const gdouble *data = field->data;
+    gdouble qc = 1.0/(max - min);
+
+    // TODO: If width = xto - xfrom and height = yto - yfrom do not interpolate.
+    InterpolationPoint *interpx = build_interpolation(width, xfrom, xto, xres),
+                       *interpy = build_interpolation(height, yfrom, yto, yres);
+
+    InterpolationPoint *ipy = interpy;
+    for (guint i = 0; i < height; i++, ipy++) {
+        guchar *pixrow = pixels + i*rowstride;
+        const gdouble *rowprev = data + xres*ipy->prev,
+                      *rownext = data + xres*ipy->next;
+        gdouble wyp = ipy->wprev, wyn = ipy->wnext;
+        InterpolationPoint *ipx = interpx;
+
+        for (guint j = 0; j < width; j++, ipx++) {
+            guint iprev = ipx->prev, inext = ipx->next;
+            gdouble wxp = ipx->wprev, wxn = ipx->wnext;
+            gdouble z = ((rowprev[iprev]*wxp + rowprev[inext]*wxn)*wyp
+                         + (rownext[iprev]*wxp + rownext[inext]*wxn)*wyn);
+
+            z = qc*(z - min);
+            z = CLAMP(z, 0.0, 1.0);
+            // TODO: Inline the colour calculation
+            // TODO: Support pixmaps with alpha
+            GwyRGBA rgba;
+            gwy_gradient_color(gradient, z, &rgba);
+            gwy_rgba_to_rgb8_pixel(&rgba, pixrow);
+            pixrow += 3;
+        }
+    }
+
+    g_free(interpy);
+    g_free(interpx);
 }
 
 /**
