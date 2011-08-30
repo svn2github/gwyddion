611edfd014c1c7d3573c53a4349d590de513f308
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-08-30 07:22:46 +0000

    - added tests for field part intersection and union
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@12776 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/field-part.c
+++ branches/GWYDDION-3/libgwy/tests/field-part.c
@@ -48,4 +48,95 @@ test_field_part_boxed(void)
     g_boxed_free(GWY_TYPE_FIELD_PART, copy);
 }
 
+void
+test_field_part_intersect(void)
+{
+    enum { max_size = 40, niter = 500, n = 10000 };
+    GRand *rng = g_rand_new_with_seed(42);
+
+    for (guint iter = 0; iter < niter; iter++) {
+        GwyFieldPart result = {
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+        };
+        GwyFieldPart otherpart = {
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+        };
+        GwyFieldPart fpart = result;
+        gboolean intersecting = gwy_field_part_intersect(&result, &otherpart);
+
+        for (guint k = 0; k < n; k++) {
+            guint j = g_rand_int_range(rng, 0, 2*max_size);
+            guint i = g_rand_int_range(rng, 0, 2*max_size);
+            gboolean in_fpart = (j >= fpart.col
+                                 && j - fpart.col < fpart.width
+                                 && i >= fpart.row
+                                 && i - fpart.row < fpart.height);
+            gboolean in_otherpart = (j >= otherpart.col
+                                     && j - otherpart.col < otherpart.width
+                                     && i >= otherpart.row
+                                     && i - otherpart.row < otherpart.height);
+            gboolean in_result = (j >= result.col
+                                  && j - result.col < result.width
+                                  && i >= result.row
+                                  && i - result.row < result.height);
+
+            g_assert_cmpuint(in_result, ==, in_fpart && in_otherpart);
+            g_assert(intersecting || !in_result);
+        }
+    }
+
+    g_rand_free(rng);
+}
+
+void
+test_field_part_union(void)
+{
+    enum { max_size = 40, niter = 500, n = 10000 };
+    GRand *rng = g_rand_new_with_seed(42);
+
+    for (guint iter = 0; iter < niter; iter++) {
+        GwyFieldPart result = {
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+        };
+        GwyFieldPart otherpart = {
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+            g_rand_int_range(rng, 0, max_size),
+        };
+        GwyFieldPart fpart = result;
+        gwy_field_part_union(&result, &otherpart);
+
+        for (guint k = 0; k < n; k++) {
+            guint j = g_rand_int_range(rng, 0, 2*max_size);
+            guint i = g_rand_int_range(rng, 0, 2*max_size);
+            gboolean in_x = ((j >= fpart.col
+                              || j >= otherpart.col)
+                             && (j < fpart.col + fpart.width
+                                 || j < otherpart.col + otherpart.width));
+            gboolean in_y = ((i >= fpart.row
+                              || i >= otherpart.row)
+                             && (i < fpart.row + fpart.height
+                                 || i < otherpart.row + otherpart.height));
+            gboolean in_result = (j >= result.col
+                                  && j - result.col < result.width
+                                  && i >= result.row
+                                  && i - result.row < result.height);
+
+            g_assert_cmpuint(in_result, ==, in_x && in_y);
+        }
+    }
+
+    g_rand_free(rng);
+}
+
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
