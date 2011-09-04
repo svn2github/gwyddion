3d0b8b2399e2609a9f010e1a49a3c44388009b9f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-09-04 10:45:26 +0000

    - corrected wrong w1 when shortening the last scaling segment
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@12814 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/mask-line.c
+++ branches/GWYDDION-3/libgwy/mask-line.c
@@ -592,7 +592,7 @@ _gwy_mask_prepare_scaling(gdouble pos, gdouble step, guint nsteps,
     seg--;
     if (seg->move && seg->w1 < 1e-6) {
         seg->move--;
-        seg->w1 = seg->move ? 1.0 : 0.0;
+        seg->w1 = seg->move ? 1.0/step : 0.0;
         end--;
     }
 
