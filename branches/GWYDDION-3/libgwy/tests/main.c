5dd5c82273dcd02f2a7deef35c7bdea718db1772
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-03-16 19:49:21 +0000

    - added a simple test for gwy_n_cpus() just to execute it
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@13334 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ branches/GWYDDION-3/libgwy/tests/main.c
@@ -0,0 +1,39 @@
+/*
+ *  $Id$
+ *  Copyright (C) 2012 David Nečas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "testlibgwy.h"
+
+/***************************************************************************
+ *
+ * Main
+ *
+ ***************************************************************************/
+
+void
+test_get_n_cpus(void)
+{
+    guint ncpus = gwy_n_cpus();
+    g_assert_cmpuint(ncpus, >=, 1);
+    // If this does not hold we are in deep shit even if gwy_n_cpus() tells the
+    // truth.
+    g_assert_cmpuint(ncpus, <=, 2 << 24);
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
+
