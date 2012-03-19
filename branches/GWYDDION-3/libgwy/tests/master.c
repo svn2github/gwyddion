5f2570984e18e58c8b4b6f117501a22165564a5a
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-03-19 11:18:44 +0000

    - added cancellation tests
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@13346 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/master.c
+++ branches/GWYDDION-3/libgwy/tests/master.c
@@ -17,6 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
 #include "testlibgwy.h"
 
 /***************************************************************************
@@ -85,7 +86,7 @@ static void
 master_sum_numbers_one(guint nproc)
 {
     GError *error = NULL;
-    enum { niter = 1000 };
+    enum { niter = 200 };
     GRand *rng = g_rand_new_with_seed(42);
 
     //g_test_timer_start();
@@ -156,6 +157,130 @@ void
 test_master_sum_auto(void)
 {
     master_sum_numbers_one(0);
+}
+
+enum { TASK_TIME = 200 };
+
+static gpointer
+cancel_worker(gpointer task,
+              G_GNUC_UNUSED gpointer data)
+{
+    g_assert_cmpuint(GPOINTER_TO_UINT(task), ==, 0xdeadbeefU);
+    g_usleep(TASK_TIME);
+    return GUINT_TO_POINTER(0xbaddecafU);
+}
+
+static gpointer
+cancel_task(G_GNUC_UNUSED gpointer user_data)
+{
+    return GUINT_TO_POINTER(0xdeadbeefU);
+}
+
+static void
+cancel_result(gpointer result,
+              gpointer user_data)
+{
+    g_assert_cmpuint(GPOINTER_TO_UINT(result), ==, 0xbaddecafU);
+    guint *count = (guint*)user_data;
+    (*count)++;
+}
+
+static gpointer
+cancel_cancel(gpointer user_data)
+{
+    GCancellable *cancellable = (GCancellable*)user_data;
+    g_assert(G_IS_CANCELLABLE(cancellable));
+    guint delay = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(cancellable),
+                                                     "delay"));
+    g_usleep(delay);
+    g_cancellable_cancel(cancellable);
+    return NULL;
+}
+
+void
+master_cancel_one(guint nproc)
+{
+    if (g_test_trap_fork(10000000UL, 0)) {
+        GError *error = NULL;
+        enum { niter = 200 };
+        GRand *rng = g_rand_new_with_seed(42);
+
+        for (guint iter = 0; iter < niter; iter++) {
+            GwyMaster *master = gwy_master_new();
+            guint delay = g_rand_int_range(rng, 0, 10000);
+
+            gboolean ok = gwy_master_create_workers(master, nproc, &error);
+            g_assert_no_error(error);
+            g_assert(ok);
+
+            GCancellable *cancellable = g_cancellable_new();
+            g_object_set_data(G_OBJECT(cancellable),
+                              "delay", GUINT_TO_POINTER(delay));
+            GThread *cthread = g_thread_create(&cancel_cancel,
+                                               cancellable, FALSE, &error);
+            g_assert_no_error(error);
+            g_assert(cthread);
+
+            guint count = 0;
+            ok = gwy_master_manage_tasks(master, 0, &cancel_worker,
+                                         &cancel_task, &cancel_result,
+                                         &count, cancellable);
+
+            g_object_unref(master);
+            g_object_unref(cancellable);
+
+            // The actual number of calls can differ wildly depending on the
+            // scheduling.  How to check something meaningful?
+            //gdouble expected_count = (gdouble)delay/TASK_TIME;
+        }
+        g_rand_free(rng);
+        exit(0);
+    }
+    else {
+         g_test_trap_assert_passed();
+    }
+}
+
+void
+test_master_cancel_1(void)
+{
+    master_cancel_one(1);
+}
+
+void
+test_master_cancel_2(void)
+{
+    master_cancel_one(2);
+}
+
+void
+test_master_cancel_3(void)
+{
+    master_cancel_one(3);
+}
+
+void
+test_master_cancel_4(void)
+{
+    master_cancel_one(4);
+}
+
+void
+test_master_cancel_8(void)
+{
+    master_cancel_one(8);
+}
+
+void
+test_master_cancel_16(void)
+{
+    master_cancel_one(16);
+}
+
+void
+test_master_cancel_auto(void)
+{
+    master_cancel_one(0);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
