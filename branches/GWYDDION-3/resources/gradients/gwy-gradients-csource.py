f6b8aaa0a5774f082118e24ecbe62622ca3b09d3
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-10-31 22:51:59 +0000

    - corrected generated C syntax
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@14122 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/resources/gradients/gwy-gradients-csource.py
+++ branches/GWYDDION-3/resources/gradients/gwy-gradients-csource.py
@@ -33,7 +33,7 @@ for filename in sys.argv[3:]:
 lenvar = '%s_nitems' % varname
 sys.stdout.write('enum { %s = %u };\n' % (lenvar, len(names)))
 
-sys.stdout.write('static const guint %s_lengths[%s] =\n'
+sys.stdout.write('static const guint %s_lengths[%s] = {\n'
                  % (varname, lenvar))
 sys.stdout.write('    %s\n' % (', '.join('%u' % n for n in lengths)))
 sys.stdout.write('};\n')
@@ -42,7 +42,7 @@ sys.stdout.write('static const gchar %s_names[] =\n' % varname)
 sys.stdout.write('    "%s"\n' % ('\\000"\n    "'.join(names)))
 sys.stdout.write(';\n')
 
-sys.stdout.write('static const GwyGradientPoint %s_data[] =\n'
+sys.stdout.write('static const GwyGradientPoint %s_data[] = {\n'
                  % varname)
 sys.stdout.write(',\n'.join(points) + '\n')
 sys.stdout.write('};\n')
