69cd37f7b409203f7fb893bc3bf92d0ad5355966
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-11-04 12:49:06 +0000

    - moved TRANSLATORS where it is picked up by xgettext
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@14158 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/curve.c
+++ branches/GWYDDION-3/libgwy/curve.c
@@ -258,9 +258,9 @@ gwy_curve_construct(GwySerializable *serializable,
     guint len = its[2].array_size;
     if (len && its[2].value.v_double_array) {
         if (len % 2 != 0) {
-            // TRANSLATORS: %s is a data type name, e.g. GwyCurve, GwyGradient.
             gwy_error_list_add(error_list, GWY_DESERIALIZE_ERROR,
                                GWY_DESERIALIZE_ERROR_INVALID,
+            // TRANSLATORS: %s is a data type name, e.g. GwyCurve, GwyGradient.
                                _("Data length of ‘%s’ is %lu which is not "
                                  "a multiple of %u."),
                                "GwyCurve", (gulong)its[2].array_size, 2);
