f34faa3158d885f59585d0bd82666102de31d9ba
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-11-25 13:16:36 +0000

    - added a simple linear function preservation test for   gwy_interpolation_resample_block_1d()
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@14366 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/interpolation.c
+++ branches/GWYDDION-3/libgwy/tests/interpolation.c
@@ -25,7 +25,7 @@
  *
  ***************************************************************************/
 
-void
+static void
 interpolation_constant_one(GwyInterpolationType interpolation,
                            gdouble value)
 {
@@ -83,7 +83,7 @@ interpolation_linear_long_one(GwyInterpolationType interpolation,
     // basis functions.
     guint len = 50*support_len;
 
-    gdouble data[len];
+    gdouble *data = g_new(gdouble, len);
     for (guint i = 0; i < len; i++) {
         gdouble origin = len/2 - support_len/2 - 1.0;
         data[i] = (i - origin)*(b - a) + a;
@@ -99,6 +99,8 @@ interpolation_linear_long_one(GwyInterpolationType interpolation,
                           <=,
                           1e-14 * (fabs(ivalue) + fabs(value)));
     }
+
+    g_free(data);
 }
 
 void
@@ -118,14 +120,14 @@ void
 test_interpolation_linear_interpolating(void)
 {
     // Interpolating basis.
-    GwyInterpolationType reproducing_linear1[] = {
+    GwyInterpolationType reproducing_linear[] = {
         GWY_INTERPOLATION_LINEAR,
         GWY_INTERPOLATION_KEYS,
         GWY_INTERPOLATION_SCHAUM,
     };
-    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear1); i++) {
-        interpolation_linear_one(reproducing_linear1[i], 1.0, 2.0);
-        interpolation_linear_one(reproducing_linear1[i], 1.0, -1.0);
+    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear); i++) {
+        interpolation_linear_one(reproducing_linear[i], 1.0, 2.0);
+        interpolation_linear_one(reproducing_linear[i], 1.0, -1.0);
     }
 }
 
@@ -133,15 +135,93 @@ void
 test_interpolation_linear_non_interpolating(void)
 {
     // Non-interpolating basis.
-    GwyInterpolationType reproducing_linear2[] = {
+    GwyInterpolationType reproducing_linear[] = {
         GWY_INTERPOLATION_BSPLINE4,
         GWY_INTERPOLATION_OMOMS4,
         GWY_INTERPOLATION_BSPLINE6,
     };
-    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear2); i++) {
-        interpolation_linear_long_one(reproducing_linear2[i], 1.0, 2.0);
-        interpolation_linear_long_one(reproducing_linear2[i], 1.0, -1.0);
+    for (guint i = 0; i < G_N_ELEMENTS(reproducing_linear); i++) {
+        interpolation_linear_long_one(reproducing_linear[i], 1.0, 2.0);
+        interpolation_linear_long_one(reproducing_linear[i], 1.0, -1.0);
     }
+}
+
+static void
+interpolation_resample_linear_one(GwyInterpolationType interpolation,
+                                  gdouble a, gdouble b,
+                                  guint len, guint newlen)
+{
+    guint support_len = gwy_interpolation_get_support_size(interpolation);
+    g_assert_cmpint(support_len, >, 0);
+    gboolean hinterp = gwy_interpolation_has_interpolating_basis(interpolation);
+    gdouble edge = hinterp ? 0.5*(support_len - 1.0) : 8.0*(support_len - 1.0);
+
+    // We could not compare anything at all.
+    if (0.5*len < edge)
+        return;
+
+    gdouble *data = g_new(gdouble, len);
+    for (guint i = 0; i < len;i++) {
+        gdouble x = (i + 0.5)/len;
+        data[i] = a*(1.0 - x) + b*x;
+    }
+
+    gdouble *newdata = g_new(gdouble, newlen);
+    gwy_interpolation_resample_block_1d(len, data, newlen, newdata,
+                                        interpolation, TRUE);
+
+    for (guint i = 0; i < len; i++) {
+        gdouble x = (i + 0.5)/len;
+        data[i] = a*(1.0 - x) + b*x;
+    }
+
+    for (guint i = 0; i < newlen; i++) {
+        gdouble x = (i + 0.5)/newlen;
+        // We cannot check edge values because they depend on the specific
+        // interpolation type.  For non-interpolating bases edge needs to be
+        // taken much larger.
+        gdouble iorig = x*len;
+        if (iorig > 0.5*len)
+            iorig = len - iorig;
+        if (iorig < edge)
+            continue;
+
+        gdouble expected = a*(1.0 - x) + b*x;
+        //g_printerr("[%u] %.16f %.16f :: %g\n", i, expected, newdata[i], iorig);
+        gwy_assert_floatval(newdata[i], expected, 1e-13);
+    }
+
+    g_free(newdata);
+    g_free(data);
+}
+
+void
+test_interpolation_resample_1d_linear(void)
+{
+    GwyInterpolationType reproducing_linear[] = {
+        GWY_INTERPOLATION_LINEAR,
+        GWY_INTERPOLATION_KEYS,
+        GWY_INTERPOLATION_SCHAUM,
+        GWY_INTERPOLATION_BSPLINE4,
+        GWY_INTERPOLATION_OMOMS4,
+        GWY_INTERPOLATION_BSPLINE6,
+    };
+
+    enum { niter = 1000 };
+    GRand *rng = g_rand_new_with_seed(42);
+
+    for (guint iter = 0; iter < niter; iter++) {
+        guint i = g_rand_int_range(rng, 0, G_N_ELEMENTS(reproducing_linear));
+        GwyInterpolationType interpolation = reproducing_linear[i];
+        // Generate also small initial sizes for non-interpolating bases.
+        // The test will just pass without comparing any values at all.
+        guint size = g_rand_int_range(rng, 2, 400);
+        guint newsize = g_rand_int_range(rng, 2, 400);
+        interpolation_resample_linear_one(interpolation, -1.0, 1.0,
+                                          size, newsize);
+    }
+
+    g_rand_free(rng);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
