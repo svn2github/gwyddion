1c343d41f9c52cd937f8df8c61693e614d45f6fd
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2012-12-13 22:37:36 +0000

    - added #include <coords-view.h>
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@14473 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwyui/libgwyui.h
+++ branches/GWYDDION-3/libgwyui/libgwyui.h
@@ -27,6 +27,7 @@
 #include <libgwyui/cell-renderer-gradient.h>
 #include <libgwyui/choice.h>
 #include <libgwyui/color-axis.h>
+#include <libgwyui/coords-view.h>
 #include <libgwyui/field-render.h>
 #include <libgwyui/inventory-store.h>
 #include <libgwyui/main.h>
