2baac4b41c75a3f4057d89e4e12b42799b7330f7
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-04-02 08:03:12 +0000

    - free random number generators
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@14792 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/brick.c
+++ branches/GWYDDION-3/libgwy/tests/brick.c
@@ -2098,6 +2098,8 @@ test_brick_extract_line(void)
         g_object_unref(target);
         g_object_unref(reference);
     }
+
+    g_rand_free(rng);
 }
 
 static void
@@ -2269,6 +2271,8 @@ test_brick_extract_plane(void)
         g_object_unref(target);
         g_object_unref(reference);
     }
+
+    g_rand_free(rng);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
