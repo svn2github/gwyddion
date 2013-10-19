3c3a6fb3200c903478dd17f6e2aacaff0a357e1c
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-10-19 07:56:34 +0000

    - free GString used in the append test
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@15460 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/libgwy/tests/value-format.c
+++ branches/GWYDDION-3/libgwy/tests/value-format.c
@@ -334,6 +334,8 @@ test_value_format_append(void)
     gwy_value_format_append(format, str, 1.0);
     g_assert_cmpstr(str->str, ==, "Value is 1 m");
     g_object_unref(format);
+
+    g_string_free(str, TRUE);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
