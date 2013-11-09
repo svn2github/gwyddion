94ab2eb871eb2a1b7c5e8c679db9d41ac3c9ce6f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-11-09 23:44:14 +0000

    - removed constructor overrides, it seems they are not recommended - added a simple Unit override for emptiness testing
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code@15590 25697fe4-d644-49b8-9d30-be5465f2adf3

--- branches/GWYDDION-3/python/overrides/Gwy.py
+++ branches/GWYDDION-3/python/overrides/Gwy.py
@@ -7,34 +7,18 @@ Gwy = get_introspection_module('Gwy')
 
 __all__ = []
 
-class Field(Gwy.Field):
-    def __new__(klass, *args, **kwargs):
-        if not args:
-            return Gwy.Field()
+def match_signature(args, types):
+    if len(args) != len(types):
+        return False
 
-        n = len(args)
-        t0 = type(args[0])
-        if n == 1 and issubclass(t0, Gwy.MaskField):
-            return Gwy.Field.new_from_mask(args[0], 0.0, 1.0)
-        if n == 2 and issubclass(t0, Gwy.Field):
-            return Gwy.Field.new_alike(*args)
-        if n == 3 and issubclass(t0, Gwy.MaskField):
-            return Gwy.Field.new_from_mask(*args)
-        if n == 3 and issubclass(t0, int):
-            return Gwy.Field.new_sized(*args)
-        if n == 3 and issubclass(t0, Gwy.Field):
-            t2 = type(args[2])
-            if issubclass(t2, Gwy.PlaneCongruence):
-                return Gwy.Field.new_congruent(*args)
-            return Gwy.Field.new_part(*args)
-        if n == 4 and issubclass(t0, Gwy.Field):
-            return Gwy.Field.new_resampled(*args)
+    for pair in zip(args, types):
+        if not issubclass(pair[0], pair[1]):
+            return False
+    return True
 
-        raise TypeError('Wrong number or type of constructor arguments')
+class Unit(Gwy.Unit):
+    def __nonzero__(self):
+        return not self.is_empty()
 
-    def __init__(self, *args, **kwargs):
-        super(Field, self).__init__(**kwargs)
-
-Field = override(Field)
-__all__.append('Field')
-
+Unit = override(Unit)
+__all__.append('Unit')
